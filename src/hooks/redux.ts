a2eb717daea9b1639f03f5f2368cdd5cdab375a1
Sam Davidoff <sam@mfbtech.com>
2025-05-30 20:28:50 +0000

    fix linting issue

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -1,5 +1,7 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from '../store/createAppStore';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
