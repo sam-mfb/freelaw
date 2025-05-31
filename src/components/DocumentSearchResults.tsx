fc13eeced31e64cee2f43385f4b4695ac7868b9a
Sam Davidoff <sam@mfbtech.com>
2025-05-31 18:49:42 +0000

    format changes

--- src/components/DocumentSearchResults.tsx
+++ src/components/DocumentSearchResults.tsx
@@ -62,10 +62,7 @@ export const DocumentSearchResults: React.FC = () => {
 
       <div className="results-list">
         {results.map((document) => (
-          <div
-            key={document.id}
-            className="document-result-card"
-          >
+          <div key={document.id} className="document-result-card">
             <div className="document-header">
               <h4 className="document-title">
                 <span
@@ -97,7 +94,7 @@ export const DocumentSearchResults: React.FC = () => {
               {document.pageCount && <span>{document.pageCount} pages</span>}
               {document.fileSize && <span>{(document.fileSize / 1024).toFixed(1)} KB</span>}
             </div>
-            
+
             {document.filePath && (
               <div className="document-actions">
                 <button
